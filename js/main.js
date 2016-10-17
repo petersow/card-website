0e3e0ce46e6f3943bfce6f756a0e0428a6977aa7
petersow <peter.sowerbutts@pressassociation.com>
2016-10-18 00:54:10 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -6,13 +6,20 @@ require.config({
 		angular: '../lib/angular',
 		angularrouter: '../lib/angular-route',
 		jquery: '../lib/jquery',
-		bootstrap: '../lib/bootstrap'
+		bootstrap: '../lib/bootstrap',
+		ngtable: 'external/ng-table'
+	},
+	shim: {
+		'angular': { exports: 'angular' },
+		'angular-route': ['angular'],
+		'ngTable': ['angular']
 	}
 });
+
 window.app = {};
-require(['angular', 'jquery'], function() {
+require(['angular', 'jquery', 'ngtable'], function() {
 	require(['angularrouter', 'bootstrap'], function() {
-		window.app = angular.module('App', ['ngRoute']);
+		window.app = angular.module('App', ['ngRoute', 'ngTable']);
 		require(['router'], function() {
 			angular.bootstrap(document, ['App']);
 		});
